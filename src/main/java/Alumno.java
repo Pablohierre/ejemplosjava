3f63799f88917a62f20c8f051f3dbd5a586f578a
Pablo <pablo.ma39215@cesurformacion.com>
2021-09-22 13:07:59 +0200

    git commit 2

---
+++ src/main/java/Alumno.java
@@ -0,0 +1,30 @@
+
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author hierr
+ */
+public class Alumno{
+    public Alumno (Integer id, String Nombre){
+    Integer id0;
+    String nombre; 
+    }
+    private Alumno Alumno;
+
+    public Alumno getAlumno() {
+        return Alumno;
+    }
+
+    public void setAlumno(Alumno Alumno) {
+        this.Alumno = Alumno;
+    }
+
+    
+}
